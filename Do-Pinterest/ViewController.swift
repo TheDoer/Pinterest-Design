734e32a7e18abe8a53da809a585aafd4338cfa10
Adonis Rumbwere <adonis.rumbwere@gmail.com>
2019-08-08 16:45:01 +0200

    added swift data source

--- Do-Pinterest/ViewController.swift
+++ Do-Pinterest/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Outlets
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       // view.backgroundColor = .white
     }
 
 
 }
+
+//MARK: Data Source
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath)
+        return cell
+    }
+}
+
 
